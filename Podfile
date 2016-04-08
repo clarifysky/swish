2d3d68d2d7e4dde393b240d89140776733c60659
gitHomeCL <cl0067@126.com>
2016-04-08 13:15:18 +0800

    initialized

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+target "swish" do
+	pod 'Alamofire', '~> 3.0'
+	pod 'SDWebImage', '~>3.7'
+	pod 'MJRefresh'
+	pod 'pop', '~> 1.0'
+end
---
+++ swish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swish
+//
+//  Created by Rex Tsao on 7/4/2016.
+//  Copyright © 2016 rexcao.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
